c1da0bc50491f011b583c521b917ca90438a7a1d
Martin Tarjanyi <tmartin193@gmail.com>
2019-12-14 19:47:32 +0100

    Switch to springdoc

--- src/main/java/com/example/openapiuiissue/DemoController.java
+++ src/main/java/com/example/openapiuiissue/DemoController.java
@@ -3,14 +3,21 @@ package com.example.openapiuiissue;
 import lombok.Data;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/demo")
 public class DemoController
 {
-    @GetMapping
+    @GetMapping("1")
     public String demo(MyParameterObject parameterObject)
+    {
+        return "demo";
+    }
+
+    @GetMapping("2")
+    public String demo2(@RequestParam String name, @RequestParam int age)
     {
         return "demo";
     }
